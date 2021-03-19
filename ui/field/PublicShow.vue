dbcaf1cfb094b213eeb2bf345f869646f7863f57
Charles Page <charliepage88@gmail.com>
2021-03-19 14:24:07 -0400

    vue 3 upgrade

--- ui/field/PublicShow.vue
+++ ui/field/PublicShow.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <template v-if="!isMultiple">
-      <a :href="value" target="_blank" rel="noopener">
-        <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <a v-if="value" :href="value" target="_blank" rel="noopener">
+        <svg class="inline-block w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
         </svg>
         <span>{{ field.name }}</span>
@@ -13,7 +13,7 @@
       <div class="flex flex-col">
         <div v-for="(file, index) in files" :key="`file-${index}`" class="w-auto mb-2">
           <a :href="file" target="_blank" rel="noopener">
-            <svg class="w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <svg class="inline-block w-4 h-4 mr-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
             </svg>
             <span>{{ field.name }} #{{ index }}</span>
